9cb143e924bd965575eccb104550189ed033890e
mouny <mouny@Mounya.lan>
2026-01-02 21:27:44 +0530

    updated browser headless mode

--- src/main/java/com/demoshopp/base/DriverManager.java
+++ src/main/java/com/demoshopp/base/DriverManager.java
@@ -2,9 +2,11 @@ package com.demoshopp.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.firefox.FirefoxOptions;
 import com.demoshopp.enums.BrowserType;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,7 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class DriverManager {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-   
+
     public static WebDriver getDriver() {
         return driver.get();
     }
@@ -22,9 +24,9 @@ public class DriverManager {
     }
 
     public static void initDriver(String browser) {
-    	 WebDriver webDriver;
+        WebDriver webDriver;
         BrowserType browserType;
-       
+
         try {
             browserType = BrowserType.valueOf(browser.toUpperCase());
         } catch (IllegalArgumentException e) {
@@ -35,24 +37,37 @@ public class DriverManager {
 
             case CHROME:
                 WebDriverManager.chromedriver().setup();
-                webDriver = new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                webDriver = new ChromeDriver(chromeOptions);
                 break;
 
             case FIREFOX:
                 WebDriverManager.firefoxdriver().setup();
-                webDriver = new FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--disable-gpu");
+                firefoxOptions.addArguments("--window-size=1920,1080");
+                webDriver = new FirefoxDriver(firefoxOptions);
                 break;
-
             case EDGE:
                 try {
                     WebDriverManager.edgedriver().setup();
-                    webDriver = new EdgeDriver();
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    edgeOptions.addArguments("headless");
+                    edgeOptions.addArguments("disable-gpu");
+                    edgeOptions.addArguments("window-size=1920,1080");
+                    webDriver = new EdgeDriver(edgeOptions);
                 } catch (Exception e) {
-                    System.setProperty(
-                        "webdriver.edge.driver",
-                        System.getProperty("user.dir") + "/drivers/msedgedriver.exe"
-                    );
-                    webDriver = new EdgeDriver();
+                    System.setProperty("webdriver.edge.driver",
+                        System.getProperty("user.dir") + "/drivers/msedgedriver.exe");
+                    EdgeOptions fallbackOptions = new EdgeOptions();
+                    fallbackOptions.addArguments("headless");
+                    fallbackOptions.addArguments("disable-gpu");
+                    fallbackOptions.addArguments("window-size=1920,1080");
+                    webDriver = new EdgeDriver(fallbackOptions);
                 }
                 break;
 
@@ -60,7 +75,6 @@ public class DriverManager {
                 throw new IllegalStateException("Unexpected browser: " + browserType);
         }
 
-        webDriver.manage().window().maximize();
         setDriver(webDriver);
     }
 
