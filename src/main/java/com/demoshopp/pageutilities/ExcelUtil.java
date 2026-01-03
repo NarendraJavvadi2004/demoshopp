06deda6122f6aa126dbe0b8a3f8ec96aba178d6e
NarendraJavvadi2004 <mounyajavvadi@gmail.com>
2026-01-03 19:50:35 +0530

    updated

--- src/main/java/com/demoshopp/pageutilities/ExcelUtil.java
+++ src/main/java/com/demoshopp/pageutilities/ExcelUtil.java
@@ -2,42 +2,37 @@ package com.demoshopp.pageutilities;
 
 import org.apache.poi.ss.usermodel.*;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class ExcelUtil {
 
-    public static Object[][] getSheetData(String filePath, String sheetName) {
+	public static Object[][] getSheetData(String fileName, String sheetName) {
+	    Object[][] data;
+	    try (InputStream inputStream = ExcelUtil.class.getClassLoader().getResourceAsStream(fileName);
+	         Workbook workbook = WorkbookFactory.create(inputStream)) {
 
-        Object[][] data;
+	        Sheet sheet = workbook.getSheet(sheetName);
+	        if (sheet == null) {
+	            throw new RuntimeException("Sheet '" + sheetName + "' not found in " + fileName);
+	        }
 
-        try (FileInputStream fis = new FileInputStream(filePath);
-             Workbook workbook = WorkbookFactory.create(fis)) {
+	        int rowCount = sheet.getPhysicalNumberOfRows();
+	        int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+	        data = new Object[rowCount - 1][colCount];
 
-            Sheet sheet = workbook.getSheet(sheetName);
-
-            int rowCount = sheet.getPhysicalNumberOfRows();
-            int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
-
-            data = new Object[rowCount - 1][colCount];
-
-            for (int i = 1; i < rowCount; i++) {
-                Row row = sheet.getRow(i);
-
-                for (int j = 0; j < colCount; j++) {
-                    Cell cell = row.getCell(j);
-
-                    if (cell == null) {
-                        data[i - 1][j] = "";
-                    } else {
-                        data[i - 1][j] = cell.toString();
-                    }
-                }
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to read Excel data", e);
-        }
-
-        return data;
-    }
-}
+	        for (int i = 1; i < rowCount; i++) {
+	            Row row = sheet.getRow(i);
+	            for (int j = 0; j < colCount; j++) {
+	                Cell cell = row.getCell(j);
+	                if (cell == null) {
+	                    data[i - 1][j] = "";
+	                } else {
+	                    data[i - 1][j] = cell.toString().trim();
+	                }
+	            }
+	        }
+	    } catch (Exception e) {
+	        throw new RuntimeException("Failed to read Excel data", e);
+	    }
+	    return data;
+	}}
--- src/test/java/com/demoshopp/tests/DataP.java
+++ src/test/java/com/demoshopp/tests/DataP.java
@@ -5,18 +5,13 @@ import org.testng.annotations.DataProvider;
 import com.demoshopp.pageutilities.ExcelUtil;
 
 public class DataP {
-  String DataPath = System.getProperty("user.dir") + "/src/test/resources/TestData.xlsx";
-	 @DataProvider(name = "loginData")
-	    public Object[][] getLoginData() {
-	        // Reads all rows from TestData.xlsx, LoginSheet
-	        return ExcelUtil.getSheetData(DataPath, "LoginSheet");
-	    }
-	 
-	 
-	 @DataProvider(name = "registerData")
-	    public Object[][] getRegisterData() {
-	        // Reads all rows from TestData.xlsx, Sheet1
-	        return ExcelUtil.getSheetData(DataPath, "RegisterSheet");
-	    }
+    @DataProvider(name = "loginData")
+    public Object[][] getLoginData() {
+        return ExcelUtil.getSheetData("TestData.xlsx", "LoginSheet");
+    }
 
+    @DataProvider(name = "registerData")
+    public Object[][] getRegisterData() {
+        return ExcelUtil.getSheetData("TestData.xlsx", "RegisterSheet");
+    }
 }
